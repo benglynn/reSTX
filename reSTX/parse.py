6dc7eddb1228e643fe8dd7ff183c538ef41516ad
Ben Glynn <benglynn@benglynn.net>
2011-08-18 07:43:40 +0100

    Parse with lxml, referencing the DTD.

--- reSTX/parse.py
+++ reSTX/parse.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
 import sys
+from StringIO import StringIO
 from docutils.core import publish_string
-import lxml
+from lxml import etree
 
 try:
     filename = sys.argv[1]
@@ -10,8 +11,12 @@ except IndexError:
     print 'Usage: %s <filename>' % sys.argv[0]
     sys.exit(1)
 
+# Convert the reST file to xml
 file = open(filename, 'r')
 rst = unicode(file.read()).encode('utf-8')
 xml = publish_string(rst, writer_name='xml')
 
-print xml
+# Parse the xml
+parser = etree.XMLParser(dtd_validation=True, no_network=False)
+tree = etree.parse(StringIO(xml), parser)
+print etree.tostring(tree.getroot())
